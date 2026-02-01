c042903d2129238afc492420f64e33a69f946dc2
Abhijith <abhijithemes@gmail.com>
2026-02-01 09:12:30 +0530

    Create Datatypes.go

---
+++ Datatypes.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b bool = true
+	var c string = "hello"
+	var f float64 = 3.14
+	var g string = "world is so big"
+	var h uint = 20
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
+}
